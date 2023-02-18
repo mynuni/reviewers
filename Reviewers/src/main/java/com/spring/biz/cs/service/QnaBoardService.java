c4bac954e57036877b7d049f9cfdcb1db341f9d4
Moon <elelaka0000@gmail.com>
2023-02-18 23:21:16 +0900

    Q&A service, dao 인터페이스 추가

---
+++ Reviewers/src/main/java/com/spring/biz/cs/service/QnaBoardService.java
@@ -0,0 +1,32 @@
+package com.spring.biz.cs.service;
+
+import java.util.List;
+
+import com.spring.biz.cs.vo.QnaBoardVO;
+
+public interface QnaBoardService {
+
+	// 게시글 목록 조회
+	List<QnaBoardVO> getQnaBoardList(String searchType, String keyword);
+
+	// 게시글 목록 전체 수
+	int getQnaBoardCount(String searchType, String keyword);
+
+	// 조회수
+	void updateQnaBoardViewCnt(int boardId);
+
+	// 게시글 등록
+	void insertQnaBoard(QnaBoardVO qnaBoardVO);
+
+	// 게시글 수정
+	void updateQnaBoard(QnaBoardVO qnaBoardVO);
+
+	// 게시글 삭제
+	void deleteQnaBoard(int boardId);
+
+	// 게시글 조회
+	QnaBoardVO getQnaBoard(int boardId);
+
+	// 파일 경로
+	String getFilePath(int boardId);
+}
